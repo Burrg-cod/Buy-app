6c07a269153c0f0a20d783f0e00dfd6bb4820def
Joao Marcos Marques De Oliveira <jmoliveira9@topazevolution.com>
2026-05-19 11:49:03 -0300

    feat: implement Input Component in Home

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,14 +1,17 @@
 import { View, Image } from "react-native";
 import styles from "./styles";
+
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+
 export default function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+      <Input placeholder="O que você precisa comprar?" />
+
       <Button title="Entrar" />
-      <Button title="Criar Conta" />
-      <Button title="Salvar" />
-      <Button title="Voltar" />
     </View>
   );
 }

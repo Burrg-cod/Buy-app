1cafedd784471dc1b7c5ede5bec76835aa867a90
Joao Marcos Marques De Oliveira <jmoliveira9@topazevolution.com>
2026-05-19 11:35:45 -0300

    feat: implement Button component in Home with entry points (Login, Sign Up, Save, Back)

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,10 +1,14 @@
 import { View, Image } from "react-native";
 import styles from "./styles";
-
+import { Button } from "@/components/Button";
 export default function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
+      <Button title="Entrar" />
+      <Button title="Criar Conta" />
+      <Button title="Salvar" />
+      <Button title="Voltar" />
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -2,8 +2,11 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#D0D2D8",
+    paddingTop: 62,
+    padding: 24,
+    gap: 24,
   },
   logo: {
     height: 34,

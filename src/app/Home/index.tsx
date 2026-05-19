5b9f8503e97e232be8bf9dd38175fad03ec6ab60
Joao Marcos Marques De Oliveira <jmoliveira9@topazevolution.com>
2026-05-19 12:18:14 -0300

    feat: integrate Button component into Home screen and adjust layout

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -3,15 +3,23 @@ import styles from "./styles";
 
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { FilterStatus } from "@/types/FilterStatus";
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-      <Input placeholder="O que você precisa comprar?" />
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
 
-      <Button title="Entrar" />
+        <Button title="Entrar" />
+      </View>
+      <View style={styles.content}>
+        <Filter status={FilterStatus.DONE} isActive />
+        <Filter status={FilterStatus.PENDING} isActive={false} />
+      </View>
     </View>
   );
 }
